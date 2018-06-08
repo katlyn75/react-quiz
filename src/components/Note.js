953c3da4c356901454b9661fe1fb01f78b88a6d3
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-08 13:13:42 -0700

    initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Note extends Component  {
+
+  render() {
+    return (
+      <div>
+        <li>Here is</li>
+        <li>a</li>
+        <li>list</li>
+        <li>item</li>
+      </div>
+    );
+  }
+
+
+}
---
+++ src/components/Notes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Note from './Note';
+
+export default class Notes extends Component {
+
+  render() {
+    return (
+      <div className = "list-container">
+        <ul>
+          <Note/>
+        </ul>
+      </div>
+    );
+  }
+
+}
