86a48f5e75c8117716aaf8aa44d21cbfa517ca16
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-08 13:21:56 -0700

    built out the notes component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Note from './Note';
 
-export default class App extends Component {
+export default class Notes extends Component {
+
+  static propTypes = {
+    notes: PropTypes.array
+  };
 
   render() {
+    const { notes } = this.props;
+
     return (
-      <div>
-        <Note/>
-      </div>
+      <ul>
+        {notes.map(note => (
+          <Note key={note}/>
+        ))}
+      </ul>
     );
   }
 }
