d45257bf49446197e55366f2b150be97a6762c51
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-08 13:41:13 -0700

    working on add note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const defaultState = {
+  text: '',
+  date: ''
+};
+
 export default class AddNote extends Component {
 
-  render() {
+    handleChange = ({ target }) => {
+      this.setState({ [target.placeholder]: target.value });
+    };
+    
+    handleSubmit = event => {
+      event.preventDefault();
+      this.props.onComplete(this.state);
+      this.setState(defaultState);
+    };
 
-    return (
-      <form>
-        <input placeholder="text" value="text" onChange={this.handleChange}/>
-        <input type="date" placeholder="date" value="date" onChange={this.handleChange}/>
-        <button type="submit" >submit</button>
-      </form>
-    );
-  }
+    render() {
+
+
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder="text" value="text" onChange={this.handleChange}/>
+          <input type="date" placeholder="date" value="date" onChange={this.handleChange}/>
+          <button type="submit" >submit</button>
+        </form>
+      );
+    }
 }
